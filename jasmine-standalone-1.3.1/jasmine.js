88bffdb73ac552d1da3e04b7c88b9a683aa7fae5
jorgecas <tres.uno@gmail.com>
2013-05-21 20:39:00 -0300

    Para poder ejecutar los test de Jasmine desde Teamcity

--- jasmine-standalone-1.3.1/jasmine.js
+++ jasmine-standalone-1.3.1/jasmine.js
@@ -20,7 +20,7 @@ page.open(url, function (status) { //Page is loaded!
         window.setTimeout(function () {
             page.render("output.png");
             phantom.exit();
-        }, 200);
+        }, 2000);
     }
 });
 console.log('Done');
