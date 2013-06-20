3a3a4611ea6a654b122feb33efdbbebedb09fb4f
jorgecas <tres.uno@gmail.com>
2013-06-19 21:23:00 -0300

    Test componente Evaluacion
    Signed-off-by: jorgecas <tres.uno@gmail.com>

--- WebAsistencia/WebRH/Scripts/ControlEvaluacion.js
+++ WebAsistencia/WebRH/Scripts/ControlEvaluacion.js
@@ -5,16 +5,16 @@
     this.calificacion = calificacion;
     this.fecha = fecha;
     
-    this.calificacion_otorgada = function () {
+    this.calificacionOtorgada = function () {
         return this.calificacion;
     }
-    this.cambiar_calificacion_otorgada = function (calificacion) {
+    this.cambiarCalificacionOtorgada = function (calificacion) {
         this.calificacion = calificacion;
     }
-    this.fecha_de_calificacion = function () {
+    this.fechaDeCalificacion = function () {
         return this.fecha;
     }
-    this.cambiar_fecha_de_calificacion = function (fecha) {
+    this.cambiarFechaDeCalificacion = function (fecha) {
         this.fecha = fecha;
     }
 
@@ -26,34 +26,42 @@
         div_contenedor.css('height: 50px;');
         div_contenedor.css('display: block;');
 
-        div_contenedor.append(crear_textbox_fecha());
-        div_contenedor.append(crear_select_calificacion());
-        return div_contenedor.html();
+        div_contenedor.append(crearSelectCalificacion());
+        div_contenedor.append(crearTextboxFecha());
+        return div_contenedor;
     }
 
-    function crear_textbox_fecha() {
+    function crearTextboxFecha() {
         var selector_de_fecha = $('<input>');
         selector_de_fecha.attr('type', 'text');
         selector_de_fecha.attr('class', 'date_picker');
-
         selector_de_fecha.attr('id', 'txt_fecha_evaluacion_' + _this.id);
-        selector_de_fecha.attr('value', _this.fecha_de_calificacion());
+		selector_de_fecha.datepicker().datepicker("option", "dateFormat", "dd/mm/yy");
+		selector_de_fecha.val(_this.fechaDeCalificacion());
+		
+		selector_de_fecha.change(function(){
+			_this.cambiarFechaDeCalificacion($(this).val());
+		});
+		
         return selector_de_fecha;
     }
 
-    function crear_select_calificacion() {
+    function crearSelectCalificacion() {
         var selector_de_calificacion = $('<select>');
 
         selector_de_calificacion.attr("id", "cmb_calificacion_" + _this.id);
 
         selector_de_calificacion.append($("<option>", { value: "A", text: "A" }));
         for (var i = 0; i <= 10; i++) {
-            if (i == _this.calificacion_otorgada())
+            if (i == _this.calificacionOtorgada())
                 selector_de_calificacion.append($("<option>", { value: i, text: i.toString(), "selected": "selected" }));
             else
                 selector_de_calificacion.append($("<option>", { value: i, text: i.toString()}));
             
         }
+		selector_de_calificacion.change(function(){
+			_this.cambiarCalificacionOtorgada($(this).val());
+		});
         return selector_de_calificacion;
     }
 
