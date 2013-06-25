a55a5acf7571c8f433e00ee8494160e890b0c98f
Jorge Castillo <fernandocaino84@gmail.com>
2013-06-24 22:11:32 -0300

    Checkpoint. Progreso sobre el objeto Evaluación (celda) e Instancia de Evaluación (encabezado) en Javascript.

--- jasmine-standalone-1.3.1/spec/GrillaEvaluaciones-Spec.js
+++ jasmine-standalone-1.3.1/spec/GrillaEvaluaciones-Spec.js
@@ -7,40 +7,40 @@ describe("Test Grilla de Evaluaciones", function () {
 
     it("Mi objeto evaluaciones deberia decirme la calificación otorgada", function () {
 
-        spyOn(una_evaluacion, 'calificacion_otorgada').andCallThrough();
-        spyOn(una_evaluacion, 'fecha_de_calificacion').andCallThrough();
+        spyOn(una_evaluacion, 'calificacionOtorgada').andCallThrough();
+        spyOn(una_evaluacion, 'fechaDeCalificacion').andCallThrough();
 
-        expect(una_evaluacion.calificacion_otorgada()).toBe(una_calificacion);
+        expect(una_evaluacion.calificacionOtorgada()).toBe(una_calificacion);
     });
 
     it("Mi objeto evaluaciones deberia decirme la calificación otorgada y la fecha en que se hizo", function () {
 
-        spyOn(una_evaluacion, 'calificacion_otorgada').andCallThrough();
-        spyOn(una_evaluacion, 'fecha_de_calificacion').andCallThrough();
+        spyOn(una_evaluacion, 'calificacionOtorgada').andCallThrough();
+        spyOn(una_evaluacion, 'fechaDeCalificacion').andCallThrough();
 
-        expect(una_evaluacion.calificacion_otorgada()).toBe(una_calificacion);
-        expect(una_evaluacion.fecha_de_calificacion()).toBe(una_fecha);
+        expect(una_evaluacion.calificacionOtorgada()).toBe(una_calificacion);
+        expect(una_evaluacion.fechaDeCalificacion()).toBe(una_fecha);
     });
 
     it("Debería poder cambiar la fecha de una evaluación", function () {
         var otra_fecha_de_evaluacion = "25/03/2013";
 
-        spyOn(una_evaluacion, 'fecha_de_calificacion').andCallThrough();
+        spyOn(una_evaluacion, 'fechaDeCalificacion').andCallThrough();
 
-        expect(una_evaluacion.fecha_de_calificacion()).toBe(una_fecha);
-        una_evaluacion.cambiar_fecha_de_calificacion(otra_fecha_de_evaluacion);
-        expect(una_evaluacion.fecha_de_calificacion()).toBe(otra_fecha_de_evaluacion);
+        expect(una_evaluacion.fechaDeCalificacion()).toBe(una_fecha);
+        una_evaluacion.cambiarFechaDeCalificacion(otra_fecha_de_evaluacion);
+        expect(una_evaluacion.fechaDeCalificacion()).toBe(otra_fecha_de_evaluacion);
 
     });
 
     it("Debería poder cambiar la calificación otorgada de una evaluación", function () {
         var otra_calificacion = 10;
 
-        spyOn(una_evaluacion, 'calificacion_otorgada').andCallThrough();
+        spyOn(una_evaluacion, 'calificacionOtorgada').andCallThrough();
 
-        expect(una_evaluacion.calificacion_otorgada()).toBe(una_calificacion);
-        una_evaluacion.cambiar_calificacion_otorgada(otra_calificacion);
-        expect(una_evaluacion.calificacion_otorgada()).toBe(otra_calificacion);
+        expect(una_evaluacion.calificacionOtorgada()).toBe(una_calificacion);
+        una_evaluacion.cambiarCalificacionOtorgada(otra_calificacion);
+        expect(una_evaluacion.calificacionOtorgada()).toBe(otra_calificacion);
     });
 
 });
