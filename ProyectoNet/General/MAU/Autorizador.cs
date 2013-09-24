2f4b880720cd07c80491911b3260b6e1e7054e04
acalcagno <calqui@gmail.com>
2013-09-23 21:22:24 -0300

    agregada la logica de permisos concedidos y denegados a los usuarios

---
+++ ProyectoNet/General/MAU/Autorizador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExtensionesDeLista;
+
+namespace AdministracionDeUsuarios
+{
+    public class Autorizador
+    {
+        /// <summary>
+        /// ejemplo
+        /// { "juan", { Permiso("Concedido", Funcionalidad("lectura_legajos")), Permiso("Denegado", Funcionalidad("escritura_legajos")) } }
+        /// </summary>
+        protected Dictionary<string, List<Permiso>> permisos;
+
+        public Autorizador(Dictionary<string, List<Permiso>> permisos)
+        {
+            this.permisos = permisos;
+        }
+
+        public bool PuedeAcceder(string usuario, Funcionalidad funcionalidad)
+        {
+            var permiso = PermisoPara(usuario, funcionalidad, permisos[usuario]);
+            return permiso.Permite();
+        }
+
+        protected Permiso PermisoPara(string usuario, Funcionalidad funcionalidad, List<Permiso> permisos)
+        {
+            var permisos_mas_especificos_que_this = permisos.FindAll(
+                    permiso => permiso.EsPara(funcionalidad), Permiso.Null());
+            permisos_mas_especificos_que_this.Sort((p1, p2) => { return p2.GradoDeEspecificidad() - p1.GradoDeEspecificidad(); });
+            var permiso_mas_especifico_que_this = permisos_mas_especificos_que_this.First();
+            return permiso_mas_especifico_que_this.PermisoEspecifico(funcionalidad);
+        }
+
+        public bool ConcederPermisoA(string usuario, Funcionalidad funcionalidad)
+        {
+            FuncionalidadDelUsuario(usuario).Add(Permiso.Conceder(funcionalidad));
+            return true;
+        }
+
+        public void DenegarPermisoA(string usuario, Funcionalidad funcionalidad)
+        {
+            FuncionalidadDelUsuario(usuario).Add(Permiso.Denegar(funcionalidad));
+        }
+
+        protected List<Permiso> FuncionalidadDelUsuario(string usuario)
+        {
+            if (!permisos.ContainsKey(usuario))
+                permisos.Add(usuario, new List<Permiso>());
+            return permisos[usuario];
+        }
+    }
+}
---
+++ TestAdministracionDeUsuarios/TestAutorizador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdministracionDeUsuarios;
+
+namespace TestAdministracionDeUsuarios
+{
+    [TestClass]
+    public class TestAutorizador
+    {
+        [TestMethod]
+        public void deberia_poder_tener_acceso_a_una_funcionalidad_previamente_cargada_para_un_usuario()
+        {
+            var autorizador = TestObjects.Autorizador();
+
+            Assert.IsTrue(autorizador.PuedeAcceder(Juan(), TestObjects.FuncionalidadLecturaLegajos()));
+            Assert.IsFalse(autorizador.PuedeAcceder(Juan(), TestObjects.FuncionalidadEsctrituraLegajos()));
+        }
+
+        [TestMethod]
+        public void deberia_poder_agregar_acceso_a_una_funcionalidad_nueva_a_un_usuario()
+        {
+            var autorizador = TestObjects.Autorizador();
+            autorizador.ConcederPermisoA(TestObjects.Juan(), TestObjects.FuncionalidadEsctrituraLegajos());
+
+            Assert.IsTrue(autorizador.PuedeAcceder(Juan(), TestObjects.FuncionalidadLecturaLegajos()));
+            Assert.IsTrue(autorizador.PuedeAcceder(Juan(), TestObjects.FuncionalidadEsctrituraLegajos()));
+        }
+
+        [TestMethod]
+        public void deberia_poder_agregar_una_funcionalidad_a_un_usuario_que_no_tenia_ninguna()
+        {
+            var autorizador = TestObjects.Autorizador();
+            autorizador.ConcederPermisoA(Roberto(), TestObjects.FuncionalidadLecturaLegajos());
+
+            Assert.IsTrue(autorizador.PuedeAcceder(Roberto(), TestObjects.FuncionalidadLecturaLegajos()));
+            Assert.IsFalse(autorizador.PuedeAcceder(Roberto(), TestObjects.FuncionalidadEsctrituraLegajos()));
+        }
+
+        protected string Juan()
+        {
+            return TestObjects.Juan();
+        }
+
+        protected List<Funcionalidad> SoloFuncionalidadEscrituraLegajos()
+        {
+            return new List<Funcionalidad> { FuncionalidadEsctrituraLegajos() };
+        }
+
+        protected string Roberto()
+        {
+            return "roberto";
+        }
+
+        protected Funcionalidad FuncionalidadEsctrituraLegajos()
+        {
+            return TestObjects.FuncionalidadEsctrituraLegajos();
+        }
+    }
+}
