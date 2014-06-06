15a56a3d188435b024149910f64390dc344ddbcc
belencevey <belen.cevey@gmail.com>
2014-06-05 22:17:31 -0300

    mockeo de datos en repo CV

--- ProyectoNet/General/Postular/CvCapacidadesPersonales.cs
+++ ProyectoNet/General/Postular/CvCapacidadesPersonales.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace General.Postular
+namespace General
 {
     public class CvCapacidadesPersonales
     {
--- ProyectoNet/General/Repositorios/IRepositorioDeCurriculum.cs
+++ ProyectoNet/General/Repositorios/IRepositorioDeCurriculum.cs
@@ -9,5 +9,25 @@ namespace General.Repositorios
         void ActualizarCV(CurriculumVitae cv);
         General.CurriculumVitae GetCV(int documento);
         System.Collections.Generic.List<General.CvEstudios> GetCvEstudios(int documento);
+        System.Collections.Generic.List<General.CvCapacidadesPersonales> GetCvCapacidadesPersonales(int documento);
+        System.Collections.Generic.List<General.CvCertificadoDeCapacitacion> GetCvCertificadoDeCapacitacion(int documento);
+        System.Collections.Generic.List<General.CvCompetenciasInformaticas> GetCvCompetenciasInformaticas(int documento);
+        System.Collections.Generic.List<General.CvDatosPersonales> GetCvDatosPersonales(int documento);
+        System.Collections.Generic.List<General.CvDocencia> GetCvDocencia(int documento);
+        System.Collections.Generic.List<General.CvDomicilio> GetCvDomicilio(int documento);
+        System.Collections.Generic.List<General.CvEventoAcademico> GetCvEventoAcademico(int documento);
+        System.Collections.Generic.List<General.CvExperienciaLaboral> GetCvExperienciaLaboral(int documento);
+        System.Collections.Generic.List<General.CvIdiomas> GetCvIdiomas(int documento);
+        System.Collections.Generic.List<General.CvInstitucionesAcademicas> GetCvInstitucionesAcademicas(int documento);
+        System.Collections.Generic.List<General.CvMatricula> GetCvMatricula(int documento);
+        System.Collections.Generic.List<General.CvPublicaciones> GetCvPublicaciones(int documento);
+                    
+            
+            
+            
+            
+            
+            
+            
     }
 }
