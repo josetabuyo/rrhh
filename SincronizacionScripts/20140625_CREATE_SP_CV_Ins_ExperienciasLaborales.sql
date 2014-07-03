3f7c635cc0ceb5a4e872293323a4c894bb1634ad
aazaa <arielzambrano@gmail.com>
2014-07-02 22:19:19 -0300

    scripts tablas cv

--- SincronizacionScripts/20140625_CREATE_SP_CV_Ins_ExperienciasLaborales.sql
+++ SincronizacionScripts/20140625_CREATE_SP_CV_Ins_ExperienciasLaborales.sql
@@ -1,23 +1,31 @@
-Create procedure dbo.CV_Ins_ExperienciasLaborales
-@Actividad varchar(100) = null,
-@MotivoDesvinculacion varchar(100)= null,
-@NombreEmpleador varchar(100)= null,
-@PersonasACargo varchar(100)= null,
-@PuestoOcupado varchar(100)= null,
-@TipoEmpresa varchar(100)= null,
-@FechaInicio datetime= null,
-@FechaFin datetime= null,
-@Localidad varchar(100)= null,
-@Pais varchar(100)= null,
-@Usuario int,
---@FechaOperacion, datetime,
-@Baja int= null
+Create Procedure dbo.CV_Ins_ExperienciasLaborales
+@Actividad varchar(100)=null,
+@MotivoDesvinculacion varchar(100)=null,
+@NombreEmpleador varchar(100)=null,
+@PersonasACargo varchar(100)=null,
+@PuestoOcupado varchar(100)=null,
+@TipoEmpresa varchar(100)=null,
+@FechaInicio datetime,
+@FechaFin datetime,
+@Localidad varchar(100)=null,
+@Pais varchar(100)=null,
+@Usuario int=null,
+--@FechaOperacion datetime,
+@Baja int=null,
+@Documento int =null
+
 
 AS
 
-Begin
 
-INSERT INTO [dbo].[CV_ExperienciasLaborales]
+BEGIN
+
+
+declare @IdPersona int
+select @IdPersona = id from dbo.datospersonales where nrodocumento = @Documento
+
+
+INSERT INTO [DB_RRHH].[dbo].[CV_ExperienciasLaborales]
            ([Actividad]
            ,[MotivoDesvinculacion]
            ,[NombreEmpleador]
@@ -30,35 +38,26 @@ INSERT INTO [dbo].[CV_ExperienciasLaborales]
            ,[Pais]
            ,[Usuario]
            ,[FechaOperacion]
-           ,[Baja])
+           ,[Baja]
+           ,[IdPersona])
      VALUES
-          (@Actividad,
+           (@Actividad, 
            @MotivoDesvinculacion,
            @NombreEmpleador,
            @PersonasACargo,
            @PuestoOcupado,
-           @TipoEmpresa,
+           @TipoEmpresa, 
            @FechaInicio,
            @FechaFin,
            @Localidad,
-           @Pais,
-           @Usuario,
+           @Pais, 
+           @Usuario, 
            getdate(),
-           @Baja)
-
+           @Baja,
+           @IdPersona)
 
 
 
 
 END
-
-
-
-
-
-
-
-
-
-
 
