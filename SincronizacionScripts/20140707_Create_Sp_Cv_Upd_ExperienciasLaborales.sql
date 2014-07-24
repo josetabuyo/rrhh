276d335553be04c3a8780c47543d70aca06bac47
aazaa <arielzambrano@gmail.com>
2014-07-24 20:32:29 -0300

    el campo personalACargo vuelve ooooootra vez a ser INT ¬¬

--- SincronizacionScripts/20140707_Create_Sp_Cv_Upd_ExperienciasLaborales.sql
+++ SincronizacionScripts/20140707_Create_Sp_Cv_Upd_ExperienciasLaborales.sql
@@ -1,43 +1,51 @@
-Create Procedure dbo.CV_Upd_Del_ExperienciasLaborales
-@IdExperiencia int = null,
-@Actividad varchar(100)=null,
-@MotivoDesvinculacion varchar(100)=null,
-@NombreEmpleador varchar(100)=null,
-@PersonasACargo varchar(100)=null,
-@PuestoOcupado varchar(100)=null,
-@TipoEmpresa varchar(100)=null,
-@FechaInicio datetime=null,
-@FechaFin datetime=null,
-@Localidad varchar(100)=null,
-@Pais varchar(100)=null,
-@Usuario int=null,
-@FechaOperacion datetime=null,
-@Baja int=null
+CREATE Procedure [dbo].[CV_Upd_Del_ExperienciasLaborales]  
+@IdExperienciaLaboral int = null,  
+@Actividad varchar(100)=null,  
+@MotivoDesvinculacion varchar(100)=null,  
+@NombreEmpleador varchar(100)=null,  
+@PersonasACargo int=null,  
+@PuestoOcupado varchar(100)=null,  
+@TipoEmpresa varchar(100)=null,  
+@FechaInicio datetime=null,  
+@FechaFin datetime=null,  
+@Localidad varchar(100)=null,  
+@Pais int=null,  
+@Usuario int=null,  
+@FechaOperacion datetime=null,  
+@Baja int=null,  
+@Sector varchar(100) = null    
+  
+AS  
+  
+Begin  
+  
+declare @NombreSp varchar(60)   
+ set @NombreSp = (select OBJECT_NAME(@@PROCID))  
+ exec dbo.Audit @NombreSp    
+  
+UPDATE dbo.CV_ExperienciasLaborales  
+   SET Actividad = isnull(@Actividad,Actividad),  
+      MotivoDesvinculacion = isnull(@MotivoDesvinculacion,MotivoDesvinculacion),  
+      NombreEmpleador = isnull(@NombreEmpleador,NombreEmpleador),  
+      PersonasACargo = isnull(@PersonasACargo,PersonasACargo),  
+      PuestoOcupado = isnull(@PuestoOcupado,PuestoOcupado),  
+      TipoEmpresa = isnull(@TipoEmpresa,TipoEmpresa),  
+      FechaInicio = isnull(@FechaInicio,FechaInicio),   
+      FechaFin = isnull(@FechaFin,FechaFin),  
+      Localidad = isnull(@Localidad,Localidad),   
+      Pais = isnull(@Pais, Pais),  
+      Usuario = isnull(@Usuario,Usuario),  
+      FechaOperacion = getdate(),  
+      Baja = isnull(@Baja,Baja),  
+      Sector = isnull(@Sector,Sector)  
+WHERE id=@IdExperienciaLaboral  
+  
+  
+END  
+  
+  
 
-AS
-
-Begin
-
-declare @NombreSp varchar(60) 
-	set @NombreSp = (select OBJECT_NAME(@@PROCID))
-	exec dbo.Audit @NombreSp  
-
-UPDATE dbo.CV_ExperienciasLaborales
-   SET Actividad = isnull(@Actividad,Actividad),
-      MotivoDesvinculacion = isnull(@MotivoDesvinculacion,MotivoDesvinculacion),
-      NombreEmpleador = isnull(@NombreEmpleador,NombreEmpleador),
-      PersonasACargo = isnull(@PersonasACargo,PersonasACargo),
-      PuestoOcupado = isnull(@PuestoOcupado,PuestoOcupado),
-      TipoEmpresa = isnull(@TipoEmpresa,TipoEmpresa),
-      FechaInicio = isnull(@FechaInicio,FechaInicio), 
-      FechaFin = isnull(@FechaFin,FechaFin),
-      Localidad = isnull(@Localidad,Localidad), 
-      Pais = isnull(@Pais, Pais),
-      Usuario = isnull(@Usuario,Usuario),
-      FechaOperacion = getdate(),
-      Baja = isnull(@Baja,Baja)
-WHERE id=@IdExperiencia
 
 
-END
+
 
