d0e811db131637b529be98889d3c86083818cf49
Fernando Caino <fernandocaino84@gmail.com>
2014-07-28 18:45:42 -0300

    mas postulaciones

--- ProyectoNet/General/Postular/RepositorioDePostulaciones.cs
+++ ProyectoNet/General/Postular/RepositorioDePostulaciones.cs
@@ -22,17 +22,22 @@ namespace General
         //    return puestos;
         //}
 
-        public bool PostularseA(Postulacion postulacion, MAU.Usuario usuario)
+        public Postulacion PostularseA(Postulacion postulacion, MAU.Usuario usuario)
         {
+            var idPersona = usuario.Owner.Id;
             var parametros = new Dictionary<string, object>();
             parametros.Add("@idPuesto", postulacion.IdPuesto);
-            parametros.Add("@idPersona", usuario.Owner.Id);
+            parametros.Add("@idPersona", idPersona);
             //parametros.Add("@FechaPostulacion", postulacion.FechaPostulacion);
             parametros.Add("@Motivo", postulacion.Motivo);
             parametros.Add("@Observacion", postulacion.Observaciones);
+            parametros.Add("@Usuario", usuario.Id);
 
             var id = conexion_bd.EjecutarEscalar("dbo.CV_Ins_Postulaciones", parametros);
-            return true;
+            postulacion.Id = Convert.ToInt32(id);
+            postulacion.IdPersona = idPersona;
+
+            return postulacion;
         }
     }
 }
