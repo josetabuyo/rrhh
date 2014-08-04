fece664ad7d7d6b0578ef6739a36c569d0f9da93
acalcagno <calqui@gmail.com>
2014-08-04 20:39:14 -0300

    reestructuracion de test de combos, y bindeado dinamico (con watch) del selected value.

--- WebAsistencia/WebRH/Scripts/ComboPopuladoConRepoBuilder.js
+++ WebAsistencia/WebRH/Scripts/ComboPopuladoConRepoBuilder.js
@@ -1,4 +1,61 @@
-﻿var ComboPopuladoConRepoBuilder = function (repositorio, bindings) {
+﻿
+
+/*
+ * object.watch polyfill
+ *
+ * 2012-04-03
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+ 
+// object.watch
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+ 
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
+
+var ComboPopuladoConRepoBuilder = function (repositorio, bindings) {
 	this.repositorio = repositorio;
 	this.bindings = bindings;
 };
@@ -22,13 +79,25 @@ ComboPopuladoConRepoBuilder.prototype.construirCombosEn = function (dom, binding
         };
 		
 		if (bindings != undefined) {
-			var attr = bindings[$(control).attr("Id")];
+			var attr_name = $(control).attr("Id")
+			var attr = bindings[attr_name];
 			if (typeof attr !== typeof undefined && attr !== false) {
 				parametros_constructor["id_item_seleccionado"] = attr;
+		
 			}
 		}
 		
 		var super_combo = new SuperCombo(parametros_constructor);
+		if (bindings != undefined) {
+			var attr_name = $(control).attr("Id")
+			var attr = bindings[attr_name];
+			if (typeof attr !== typeof undefined && attr !== false) {
+				bindings.watch(attr_name, function(prop, oldval, val) {
+					super_combo.id_item_seleccionado = val;
+				});
+			}
+		}
+		
 		combos.push(super_combo);
     });
 	
@@ -42,3 +111,4 @@ ComboPopuladoConRepoBuilder.prototype.construirCombosEn = function (dom, binding
 	});
 	return combos;
 };
+
