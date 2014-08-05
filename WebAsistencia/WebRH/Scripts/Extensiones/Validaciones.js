981b510b057a866948ea494d6962f5f0aebdf8f9
jlurgo <jlurgo@gmail.com>
2014-08-05 20:18:10 -0300

    129_validacion_online: listo

--- WebAsistencia/WebRH/Scripts/Extensiones/Validaciones.js
+++ WebAsistencia/WebRH/Scripts/Extensiones/Validaciones.js
@@ -2,12 +2,14 @@
     validaciones: {
         esEmailValido: {
             evaluar: function (control) {
+                if (control.val() == "") return true;
                 return control.val().length > 0 && (/^([\w\-\.]+@([\w\-]+\.)+[\w\-]{2,6})?$/).test(control.val());
             },
             mensaje: "No es un email válido"
         },
         esNumeroNatural: {
             evaluar: function (control) {
+                if (control.val() == "") return true;
                 return (/^\d+$/).test(control.val());
             },
             mensaje: "No es un número natural"
@@ -20,41 +22,66 @@
         }
     },
     esValido: function () {
-        var _this = this;
-        var esValido = true;
-        this.find("[data-validar]").each(function () {
-            var control = this;
-            var v = $(this).attr("data-validar").split(",");
-            var mensaje = "";
-            $.each(v, function (indice, valor) {
-                var res = _this.validaciones[valor].evaluar($(control));
-                if (res) {
-                    $(control).removeClass("control-invalido");
-                }
-                else {
-                    $(control).addClass("control-invalido");
-                    mensaje += ", " + _this.validaciones[valor].mensaje;
-                    esValido = res;
-                    $(control).opentip(mensaje.substring(2), {
-                        target: true,
-                        style: "alert",
-                        showOn: "creation",
-                        hideDelay: 0.2,
-                        joint: "right"
-                    });
-                }
+        var control = $(this);
+        var es_valido = true;
+
+        control.find("[data-validar]").each(function (i, sub_control) {
+            sub_control = $(sub_control);
+            sub_control.keyup(function () {
+                sub_control.aplicarValidaciones();
             });
-
-            //$(control).attr("title", mensaje.substring(2));
-
+            sub_control.change(function () {
+                sub_control.aplicarValidaciones();
+            });
+            if (!sub_control.aplicarValidaciones()) es_valido = false;
         });
-        return esValido;
+        return es_valido;
+    },
+    aplicarValidaciones: function () {
+        var control = $(this);
+        var es_valido = true;
+
+        var v = control.attr("data-validar").split(",");
+        var mensaje = "";
+        $.each(v, function (indice, valor) {
+            if (!control.validaciones[valor].evaluar(control)) {
+                mensaje += ", " + control.validaciones[valor].mensaje;
+                es_valido = false;
+            }
+        });
+        if (es_valido) {
+            control.limpiarValidaciones();
+        } else {
+            control.limpiarValidaciones();
+            control.addClass("control-invalido");
+            control.opentip(mensaje.substring(2), {
+                removeElementsOnHide: true,
+                target: true,
+                style: "alert",
+                showOn: "creation",
+                hideDelay: 0.2,
+                joint: "right"
+            });
+        }
+        return es_valido;
     },
     limpiarValidaciones: function () {
-        this.find("[data-validar]").each(function () {
-            var control = this;
-            $(control).removeClass("control-invalido");
-        });
-        for (var i = 0; i < Opentip.tips.length; i++) { Opentip.tips[i].hide(); }
+        var control = $(this);
+        var str_validaciones = control.attr("data-validar");
+        if (typeof str_validaciones !== typeof undefined && str_validaciones !== false) {
+            control.removeClass("control-invalido");
+            for (var i = 0; i < Opentip.tips.length; i++) {
+                var tip = Opentip.tips[i];
+                if (tip.triggerElement.attr("id") == control.attr("id")) tip.hide();
+            }
+        } else {
+            this.find("[data-validar]").each(function (i, sub_control) {
+                $(sub_control).removeClass("control-invalido");
+                for (var i = 0; i < Opentip.tips.length; i++) {
+                    var tip = Opentip.tips[i];
+                    if (tip.triggerElement.attr("id") == $(sub_control).attr("id")) tip.hide();
+                }
+            });
+        }
     }
 });
