1f3292986323a329c81d0e8106c793892acf0fed
aazaa <arielzambrano@gmail.com>
2014-08-21 21:54:25 -0300

    creadas las 3 tablas "satélite" con sus datos y agregadas las columnas y constraint a la tabla cv_eventosacademicos. Falta modificar 3 sp en donde se utiliza la tabla, la pantalla para que sean combos y finalmente quitar las columnas de la tabla

--- SincronizacionScripts/20140821_Normalizacion_Tabla_EventosAcademicos.sql
+++ SincronizacionScripts/20140821_Normalizacion_Tabla_EventosAcademicos.sql
@@ -1,7 +1,10 @@
---select * from CV_Instituciones
+--Sp a modificar
+--CV_GetCurriculumVitae
+--CV_Ins_EventosAcademicos
+--Cv_Upd_Del_EventosAcademicos
 
-select * from dbo.CV_EventosAcademicos
-go
+--select * from dbo.CV_EventosAcademicos
+
 
 Create Table dbo.CV_TiposDeEventoAcademico
 (
@@ -10,17 +13,63 @@ Descripcion varchar(100) unique,
 Baja Int default 0
 )
 
+go 
+
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Charla')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Coloquio')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Cursillo')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Curso')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Disertación')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Exposición')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Mesa Redonda')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Seminario')
+
+insert into dbo.CV_TiposDeEventoAcademico (Descripcion)
+values('Taller')
+
+
 go
 
-Create Table dbo.CV_EventoCaracterDeParticipacion
+Create Table dbo.CV_CaracterDeParticipacionEvento
 (
 Id Int identity(1,1) Primary Key,
 Descripcion varchar(100) unique,
 Baja Int default 0
 )
-
 go
 
+insert into dbo.CV_CaracterDeParticipacionEvento (Descripcion)
+values('Experto')
+
+insert into dbo.CV_CaracterDeParticipacionEvento (Descripcion)
+values('Invitado')
+
+insert into dbo.CV_CaracterDeParticipacionEvento (Descripcion)
+values('Moderador')
+
+insert into dbo.CV_CaracterDeParticipacionEvento (Descripcion)
+values('Orador')
+
+insert into dbo.CV_CaracterDeParticipacionEvento (Descripcion)
+values('Organizador')
+
+go
 
 Create Table dbo.CV_InstitucionesEventos
 (
@@ -29,14 +78,49 @@ Descripcion varchar(100) unique,
 Baja Int default 0
 )
 
+insert into dbo.CV_InstitucionesEventos (Descripcion)
+values('U.A.D.E.')
 
-Create Table dbo.CV_Instituciones
-(
-Id Int identity(1,1) Primary Key,
-Descripcion varchar(100) unique,
-Baja Int default 0
-)
+insert into dbo.CV_InstitucionesEventos (Descripcion)
+values('U.A.I.')
 
+insert into dbo.CV_InstitucionesEventos (Descripcion)
+values('U.B.A.')
 
+insert into dbo.CV_InstitucionesEventos (Descripcion)
+values('U.C.A.')
+
+insert into dbo.CV_InstitucionesEventos (Descripcion)
+values('U.S.A.L.')
+
+insert into dbo.CV_InstitucionesEventos (Descripcion)
+values('U.T.N.')
+
+GO
+
+alter table dbo.CV_EventosAcademicos
+Add IdTipoDeEvento int
+
+alter table dbo.CV_EventosAcademicos
+Add IdCaracterParticipacion int
+
+alter table dbo.CV_EventosAcademicos
+Add IdInstitucion int
+
+GO
+
+alter table dbo.CV_EventosAcademicos
+add constraint fk_evento_tipoevento foreign key (IdTipoDeEvento)
+references dbo.CV_TiposDeEventoAcademico (Id)
+
+alter table dbo.CV_EventosAcademicos
+add constraint fk_evento_caracter foreign key (IdCaracterParticipacion)
+references dbo.CV_CaracterDeParticipacionEvento (Id)
+
+alter table dbo.CV_EventosAcademicos
+add constraint fk_evento_institucion foreign key (IdInstitucion)
+references dbo.CV_InstitucionesEventos (Id)
+
+GO
 
 
