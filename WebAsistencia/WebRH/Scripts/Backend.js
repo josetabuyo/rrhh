ecf053b29d5b9c47063f07954155fc908a013ad9
jlurgo <jlurgo@gmail.com>
2014-09-04 21:24:46 -0300

    184_backend_sincronico: tolis, se pueden usar los metodos sincronicos haciendo backend.sync.MiMetodo

--- WebAsistencia/WebRH/Scripts/Backend.js
+++ WebAsistencia/WebRH/Scripts/Backend.js
@@ -1,5 +1,5 @@
 ﻿var Backend = {
-    start: function () {
+    start: function (on_ready) {
         var _this = this;
         this.proveedor().postearAUrl({
             url: "MetodosDelBackend",
@@ -7,12 +7,14 @@
                 for (var i = 0; i < objetos.length; i++) {
                     _this.agregarMetodo(objetos[i].nombre);
                 }
+                if(on_ready) on_ready();
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 console.log("error al obtener métodos del backend")
             }
         });
     },
+    sync: {},
     proveedor: function () {
         this._proveedor = this._proveedor || new ProveedorAjax();
         return this._proveedor;
@@ -23,12 +25,16 @@
             var promesa = new Promesa();
             _this.ejecutar(nombre_metodo, arguments, function (data) { promesa.success(data) }, function (data) { promesa.error(data) });
             return promesa;
-        }
+        };
+        this.sync[nombre_metodo] = function () {
+            return _this.ejecutarSincronico(nombre_metodo, arguments);
+        };
     },
     ejecutar: function (nombre_metodo, args, success, error) {
         var argumentos_json = [];
         for (var i = 0; i < args.length; i++) {
-            argumentos_json.push(JSON.stringify(args[i]));
+            if (typeof args[i] == "string") argumentos_json.push(args[i]);
+            else argumentos_json.push(JSON.stringify(args[i]));
         }
         this.proveedor().postearAUrl({
             url: "EjecutarEnBackend",
@@ -40,10 +46,11 @@
             error: error
         });
     },
-    ejecutarSincronico: function (nombre_metodo, args) {
+    ejecutarSincronico: function (nombre_metodo, args, on_error) {
         var argumentos_json = [];
         for (var i = 0; i < args.length; i++) {
-            argumentos_json.push(JSON.stringify(args[i]));
+            if (typeof args[i] == "string") argumentos_json.push(args[i]);
+            else argumentos_json.push(JSON.stringify(args[i]));
         }
         var respuesta;
         this.proveedor().postearAUrl({
@@ -56,8 +63,8 @@
             success: function (r) {
                 respuesta = r;
             },
-            error: function () {
-
+            error: function (error) {
+                if (on_error) on_error(error);
             }
         });
         return respuesta;
