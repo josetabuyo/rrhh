6805d04cdff1b39709bc1a7307d6957e3dc6b332
fcaino <fernandocaino84@gmail.com>
2015-01-30 20:59:02 -0300

    pre inscripcion

--- WebAsistencia/WSViaticos/App_Code/WSViaticos.cs
+++ WebAsistencia/WSViaticos/App_Code/WSViaticos.cs
@@ -2841,8 +2841,8 @@ public class WSViaticos : System.Web.Services.WebService
     public PantallaRecepcionDocumentacion GetPantallaRecepcionDocumentacion(Postulacion postulacion)
     {
         CurriculumVitae cv = RepoCurriculum().GetCV(postulacion.IdPersona);
-        RepositorioDePerfiles repoPerfiles = new RepositorioDePerfiles(Conexion());
-        repoPerfiles.GetRequisitosDelPerfil(postulacion.Perfil.Id).ForEach(r => postulacion.Perfil.Requiere(r));
+        //RepositorioDePerfiles repoPerfiles = new RepositorioDePerfiles(Conexion());
+        //repoPerfiles.GetRequisitosDelPerfil(postulacion.Perfil.Id).ForEach(r => postulacion.Perfil.Requiere(r));
 
         RepositorioDeFoliados repo = new RepositorioDeFoliados(Conexion());
         CreadorDePantallas creador = new CreadorDePantallas();
@@ -2862,11 +2862,13 @@ public class WSViaticos : System.Web.Services.WebService
 
         repo.GuardarDocumentacionRecibida(lista_doc_recibida.ToList(), usuario);
 
-        var etapas = RepoPostulaciones().GetPostulaciones().Find(p => p.Id.Equals(id_postulacion)).Etapas;//.Find(e => e.Etapa.Id.Equals(2));
+        Dictionary<string, object> parametros = new Dictionary<string, object>();
+        parametros.Add("@idPostulacion", id_postulacion);
+        var etapas = RepoPostulaciones().GetPostulaciones(parametros).First().Etapas;
 
         //VALIDO que hayan documentos para guardar y que la postulacion tenga solo la etapa de preinscripcion
-        //Le paso ETAPA 2 que es la de INSCRIPCION DOCUMENTAL
-        if (lista_doc_recibida.Count() > 0 && etapas.Count() == 1 && etapas[0].Etapa.Id.Equals(1))
+        //Le paso ETAPA 2 que es la de PREINSCRIPCION DOCUMENTAL
+        if (lista_doc_recibida.Count() > 0 && etapas.Last().Etapa.Id.Equals(ConstantesConcursar.EtapaPreinscripcionWeb.Id))
         {
             RepoPostulaciones().InsEtapaPostulacion(id_postulacion, 2, usuario.Id);
         }
