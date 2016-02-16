2c8fdc45c73bf4047f6406cbd678a397c2d150ca
jlurgo <jlurgo@gmail.com>
2016-02-16 11:18:47 -0300

    modi: preparando para probar en el server

--- ProyectoNet/General/Modi/FileSystem.cs
+++ ProyectoNet/General/Modi/FileSystem.cs
@@ -13,79 +13,63 @@ namespace General.Modi
     {
         public List<string> getPathsArchivosEnCarpeta(string path)
         {
-            //using (new Impersonator("clurgo", "SDS_DOMAIN_1", "Ma7arazzo"))
-            using (new NetworkConnection(path, new NetworkCredential("SDS_DOMAIN_1/clurgo", "Ma7arazzo")))          
+            try
             {
-                try
-                {
-                    return Directory.GetFiles(path).ToList();
-                }
-                catch (DirectoryNotFoundException e)
-                {
-                    throw new ExcepcionDeCarpetaDeLegajoNoEncontrada();
-                }
+                return Directory.GetFiles(path).ToList();
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                throw new ExcepcionDeCarpetaDeLegajoNoEncontrada();
             }
         }
 
 
-        public Image getImagenFromPath(string pathImagen)
+        public Image getImagenFromPath(string path)
         {
-            //return Image.FromFile(pathImagen);
+            //then do whatever, such as getting a list of folders:
+                
+            FileStream fs = new FileStream(path, FileMode.Open);
 
-            //using (new Impersonator("clurgo", "SDS_DOMAIN_1", "Ma7arazzo"))
-            using (new NetworkConnection(pathImagen, new NetworkCredential("SDS_DOMAIN_1/clurgo", "Ma7arazzo")))          
-            {
-                FileStream fs = new FileStream(pathImagen, FileMode.Open);
+            // allocate enough space for the file in memory
+            Byte[] b = new Byte[fs.Length];
 
-                // allocate enough space for the file in memory
-                Byte[] b = new Byte[fs.Length];
+            // read the file into memory
+            fs.Read(b, 0, b.Length);
 
-                // read the file into memory
-                fs.Read(b, 0, b.Length);
+            // close the file
+            fs.Close();
 
-                // close the file
-                fs.Close();
+            // create a new memorystream based off of the file bytes.
+            MemoryStream ms = new MemoryStream(b);
 
-                // create a new memorystream based off of the file bytes.
-                MemoryStream ms = new MemoryStream(b);
-
-                // replace Image::FromFile with Image::FromStream
-                return Image.FromStream(ms);
-            }
+            // replace Image::FromFile with Image::FromStream
+            return Image.FromStream(ms);
         }
 
-        public bool guardarImagenEnPath(string pathImagen, string bytes_imagen)
+        public bool guardarImagenEnPath(string path, string bytes_imagen)
         {
-            //using (new Impersonator("clurgo", "SDS_DOMAIN_1", "Ma7arazzo"))
-            using (new NetworkConnection(pathImagen, new NetworkCredential("SDS_DOMAIN_1/clurgo", "Ma7arazzo")))          
-            {
-                byte[] imageBytes = Convert.FromBase64String(bytes_imagen);
-                FileStream fs = new FileStream(pathImagen, FileMode.Create);
-                fs.Write(imageBytes, 0, imageBytes.Count());
-                fs.Close();
-                return true;
-            }
+            byte[] imageBytes = Convert.FromBase64String(bytes_imagen);
+            FileStream fs = new FileStream(path, FileMode.Create);
+            fs.Write(imageBytes, 0, imageBytes.Count());
+            fs.Close();
+            return true;
         }
 
-        public void moverArchivo(string pathArchivo, string pathCarpetaDestino)
+        public void moverArchivo(string path, string pathCarpetaDestino)
         {
-            //using (new Impersonator("clurgo", "SDS_DOMAIN_1", "Ma7arazzo"))
-            using (new NetworkConnection(pathArchivo, new NetworkCredential("SDS_DOMAIN_1/clurgo", "Ma7arazzo")))          
+            if (!System.IO.Directory.Exists(pathCarpetaDestino))
             {
-                if (!System.IO.Directory.Exists(pathCarpetaDestino))
-                {
-                    System.IO.Directory.CreateDirectory(pathCarpetaDestino);
-                }
-                var fileName = System.IO.Path.GetFileName(pathArchivo);
-                var destFile = pathCarpetaDestino + "/" + fileName;
-                try
-                {
-                    File.Move(pathArchivo, destFile);
-                }
-                catch (Exception e)
-                {
-                    var a = "";
-                }
+                System.IO.Directory.CreateDirectory(pathCarpetaDestino);
+            }
+            var fileName = System.IO.Path.GetFileName(path);
+            var destFile = pathCarpetaDestino + "/" + fileName;
+            try
+            {
+                File.Move(path, destFile);
+            }
+            catch (Exception e)
+            {
+                var a = "";
             }
         }
     }
