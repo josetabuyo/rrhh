8ba2ebd1c813eeda336789bf90f4129d1aebf359
jlurgo <jlurgo@gmail.com>
2016-03-01 12:36:52 -0300

    arbol: arbol completo, que sigue?

--- WebAsistencia/WebRH/Scripts/ArbolOrganigrama/ArbolOrganigrama.js
+++ WebAsistencia/WebRH/Scripts/ArbolOrganigrama/ArbolOrganigrama.js
@@ -14,7 +14,22 @@ ArbolOrganigrama.prototype.dibujarArea = function (area, contenedor, es_area_hij
     var _this = this;
     var vista_area = $("#plantillas .area_en_arbol").clone();
     var div_nombre = vista_area.find("#nombre_area");
+    var cont_areas_dependientes = vista_area.children("#areas_dependientes");
     div_nombre.text(area.nombre);
+
+    var btn_expandir = vista_area.find("#btn_expandir");
+    var btn_contraer = vista_area.find("#btn_contraer");
+    btn_expandir.click(function () {
+        cont_areas_dependientes.show();
+        btn_expandir.hide();
+        btn_contraer.show();
+    });
+    btn_contraer.click(function () {
+        cont_areas_dependientes.hide();
+        btn_contraer.hide();
+        btn_expandir.show();
+    });
+    btn_contraer.click();
     var es_area_del_usuario = _.findWhere(this.areasUsuario, { Id: area.id }) && true;
     if (es_area_del_usuario || es_area_hija_de_una_del_usuario) {
         div_nombre.addClass("area_del_usuario");
@@ -26,7 +41,14 @@ ArbolOrganigrama.prototype.dibujarArea = function (area, contenedor, es_area_hij
         });
     }
     contenedor.append(vista_area);
+    var hay_alguna_hija_en_esta_rama = false;
     _.forEach(area.areasDependientes, function (area_dep) {
-        _this.dibujarArea(area_dep, vista_area.children("#areas_dependientes"), es_area_del_usuario || es_area_hija_de_una_del_usuario);
+        if (_this.dibujarArea(area_dep, cont_areas_dependientes, es_area_del_usuario || es_area_hija_de_una_del_usuario))
+            hay_alguna_hija_en_esta_rama = true;
     });
+    if (es_area_del_usuario || es_area_hija_de_una_del_usuario || hay_alguna_hija_en_esta_rama) {
+        btn_expandir.click();
+    }
+    if (area.areasDependientes.length == 0) btn_expandir.hide();
+    return es_area_del_usuario || es_area_hija_de_una_del_usuario || hay_alguna_hija_en_esta_rama;
 };
