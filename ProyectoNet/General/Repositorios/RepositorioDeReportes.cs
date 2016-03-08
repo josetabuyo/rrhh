b907f1f64ff68a03779c5fb22c485b101e92db84
belencevey <belen.cevey@gmail.com>
2016-03-07 21:53:55 -0300

    avance graficos back

---
+++ ProyectoNet/General/Repositorios/RepositorioDeReportes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using General;
+using System.Data.SqlClient;
+
+namespace General.Repositorios
+{
+    public class RepositorioDeReportes 
+    {
+         private IConexionBD conexion_bd;
+        private List<object> datos_bd;
+        private int id_area_anterior;
+        private DateTime fecha_anterior;
+
+         public RepositorioDeReportes(IConexionBD conexion)
+        {
+            this.conexion_bd = conexion;
+        }
+
+
+         public Grafico GetGraficoDotacion(int tipo, DateTime fecha, int id_area)
+        {
+             var parametros = new Dictionary<string, object>();
+             Grafico grafico = new Grafico();
+            parametros.Add("@fechacorte", fecha);
+            parametros.Add("@id_area", id_area);
+            var tablaDatos = conexion_bd.Ejecutar("dbo.GRAF_RPT_Dotacion", parametros);
+            if (tablaDatos.Rows.Count > 0) {
+                grafico.CrearDatos(tipo, tablaDatos.Rows);
+            }
+            
+            
+            if (fecha == fecha_anterior && id_area == id_area_anterior)
+            {
+                
+            }
+            return grafico;
+            
+        }
+
+       
+    }
+}
