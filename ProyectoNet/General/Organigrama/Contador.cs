56f25da0cadec529df5463ca1f82bf13138a8ff7
belencevey <belen.cevey@gmail.com>
2016-04-08 19:37:38 -0300

    avance areas

---
+++ ProyectoNet/General/Organigrama/Contador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using General.Repositorios;
+using System.Reflection;
+
+namespace General
+{
+    public class Contador
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int Contador { get; set; }
+       
+        public Contador() { }
+
+        public Contador(int id, string descripcion, int contador)
+        {
+            this.Id = id;
+            this.Descripcion = descripcion;
+            this.Contador = contador; 
+        }
+
+    }
+}
