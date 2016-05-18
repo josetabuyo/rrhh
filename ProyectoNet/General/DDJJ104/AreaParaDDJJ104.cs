863936d7ac8d7d983223dc9d968841f7f8775092
germanranni <germanranni@gmail.com>
2016-05-18 19:50:44 -0300

    Se agrega la exportacion a Excel

--- ProyectoNet/General/DDJJ104/AreaParaDDJJ104.cs
+++ ProyectoNet/General/DDJJ104/AreaParaDDJJ104.cs
@@ -18,7 +18,8 @@ namespace General
         public DDJJ104_2001 DDJJ { get; set; }
         public AreaParaDDJJ104 AreaSuperior { get; set; }
         public string Direccion { get; set; }
-        
+        public int Jerarquia { get; set; }
+
         public AreaParaDDJJ104()
         {
             this.Personas = new List<Persona>();
