e4113d9a43cc2259325bf50cf6443ab40da1d2e8
aazaa <arielzambrano@gmail.com>
2016-09-23 21:40:27 -0300

    pantalla historial

--- WebAsistencia/WebRH/MoBi/BienesDetalle.aspx.cs
+++ WebAsistencia/WebRH/MoBi/BienesDetalle.aspx.cs
@@ -10,5 +10,21 @@ public partial class MoBi_BienesDetalle : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+
+
+    }
+
+    protected void btnMovimientos_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("HistorialBienes.aspx?idBien=" + hidden_idBien.Value);
+     
+    }
+
+
+
+    protected void btnMovimientos_Click1(object sender, EventArgs e)
+    {
+        Response.Redirect("HistorialBienes.aspx?idBien=" + hid.Value + "&MOBI_Item="+ed_descripcion_bien.InnerText);
+        
     }
 }
---
+++ WebAsistencia/WebRH/MoBi/HistorialBienes.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WSViaticos;
+public partial class MoBi_HistorialBienes : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!Page.IsPostBack)
+        {
+         //   var id_bien = Convert.ToString(Session["MOBI_IdBien"]);
+          //  txtTipoBien.Text = (string)(Session["MOBI_TipoBien"]);
+            txtItem.Text = (string)(Session["MOBI_Item"]);
+            var id =  Request.QueryString["idBien"];
+            WSViaticosSoapClient ws = new WSViaticosSoapClient();
+            Cargar_Bienes(ws, Convert.ToInt32(id));
+        }
+        
+    }
+
+    private void Cargar_Bienes(WSViaticosSoapClient ws, int IdBien)
+    {
+        WSViaticos.MoBi_Evento[] Eventos = ws.Mobi_GetEventosBien(IdBien);
+        GridViewMovimientos.DataSource = Eventos.OfType<MoBi_Evento>().ToList();
+        GridViewMovimientos.DataBind();
+    }
+
+    
+   
+
+    protected void lkBtn_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("BienesDisponibles.aspx");
+    }
+
+}
