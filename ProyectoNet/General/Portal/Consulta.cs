b930034c79aaaa8e59b977b2518833f6803b4e61
belencevey <belen.cevey@gmail.com>
2016-10-07 19:19:07 -0300

    avnce

---
+++ ProyectoNet/General/Portal/Consulta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace General
+{
+    public class Consulta
+    {
+        public int Id;
+        public Persona creador;
+        public string nombre_contestador;
+        public DateTime fechaCreacion;
+        public int tipo_id;
+        public string tipo_descripcion;
+        public string motivo;
+        public int estado_id;
+        public string estado_descripcion;
+        public Persona contestador;
+        public DateTime fechaContestacion;
+        public string respuesta;
+
+        public Consulta()
+        {
+
+        }
+
+
+    }
+}
