a78220c9cf10d423060b55de5255e8a2830a55f7
belencevey <belen.cevey@gmail.com>
2017-01-20 20:39:06 -0300

    cosas de javi que con el merge no subieron

---
+++ ProyectoNet/General/DatosAbiertos/ParametroConsultaOPD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace General.DatosAbiertos
+{
+    public class ParametroConsultaOPD
+    {
+        public int Id;
+        public string Nombre;
+        public string Valor;
+        public string Tipo;
+    }
+}
