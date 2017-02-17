9cd84799aea24219e957ab5a3cf160721e68515d
acalcagno <calqui@gmail.com>
2017-02-17 20:28:11 -0300

    se agrega el repo de evaluacion de desempenio

---
+++ ProyectoNet/General/Repositorios/RepositorioEvaluacionDesempenio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace General.Repositorios
+{
+    public class RepositorioEvaluacionDesempenio
+    {
+        private static RepositorioEvaluacionDesempenio _instancia;
+
+        IConexionBD _conexion;
+        private RepositorioEvaluacionDesempenio(IConexionBD conexion)
+        {
+            _conexion = conexion;
+        }
+
+        public static RepositorioEvaluacionDesempenio NuevoRepositorioDeLegajos(IConexionBD conexion)
+        {
+            if (!(_instancia != null)) _instancia = new RepositorioEvaluacionDesempenio(conexion);
+            return _instancia;
+        }
+
+        public string getEvaluaciones(int doc)
+        {
+            return JsonConvert.SerializeObject("hola");
+        }
+    }
+ 
+}
