0568ce3a092471f663961a66f68a09f179522bc9
msaenz <matiassaenztejeira@gmail.com>
2017-05-17 22:15:41 -0300

    actualizacion de repos tarjetones y vehiculos

--- ProyectoNet/General/MOBI/RepositorioDeVehiculos.cs
+++ ProyectoNet/General/MOBI/RepositorioDeVehiculos.cs
@@ -78,19 +78,6 @@ namespace General.Repositorios
             {
                 var row = tablaDatos.Rows[0];
 
-                /*var NumeroVehiculo = row.GetString("NumeroVehiculo", "Prueba NroVehiculo");
-                var Dominio = row.GetString("Dominio", "Prueba Dominio");
-                var Segmento = row.GetString("Segmento", "Prueba Segmento");
-                var Marca = row.GetString("Marca", "Prueba Marca");
-                var Modelo = row.GetString("Modelo", "Prueba Modelo");
-                var Motor = row.GetString("Motor", "Prueba Motor");
-                var Chasis = row.GetString("Chasis", "Prueba Chasis");
-                var Anio = row.GetString("Anio", "Prueba Anio");
-                var Observacion = row.GetString("Observacion", "Prueba Observacion");
-                var Area = row.GetString("Area", "Prueba Area");
-                var Apellido = row.GetString("Apellido", "Sin asignación");
-                var Nombre = row.GetString("Nombre", "Sin asignación");*/
-
 
                 unVehiculo.NumeroVehiculo = row.GetString("NumeroVehiculo");
                 unVehiculo.Dominio = row.GetString("Dominio");
@@ -102,9 +89,11 @@ namespace General.Repositorios
                 unVehiculo.Anio = row.GetString("Anio");
                 unVehiculo.Observacion = row.GetString("Observacion");
                 unVehiculo.Area = row.GetString("Area");
-                unVehiculo.Apellido = row.GetString("Apellido");
-                unVehiculo.Nombre = row.GetString("Nombre");
-
+                unVehiculo.Conductor = new Persona();
+                unVehiculo.Conductor.Id = row.GetInt("Persona");
+                unVehiculo.Conductor.Apellido = row.GetString("Apellido");
+                unVehiculo.Conductor.Nombre = row.GetString("Nombre");
+                unVehiculo.Conductor.IdImagen = row.GetInt("Id_Imagen_Conductor");
 
                 tablaDatos.Rows.ForEach(r => {
                     if(r.GetObject("id_imagen") is DBNull) return;
