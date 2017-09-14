f8aab880d4c42e204020d09becd8a13140b5bf2b
jlurgo <jlurgo@gmail.com>
2017-09-13 21:07:43 -0300

    acre_credenciales

---
+++ ProyectoNet/General/Repositorios/SolicitudCredencial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace General.Repositorios
+{
+    public class SolicitudCredencial
+    {
+        public int Id { get; set; }
+        public int IdPersona { get; set; }
+        public int IdMotivo { get; set; }
+
+        public SolicitudCredencial()
+        {
+                
+        }
+    }
+}
