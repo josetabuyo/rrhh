81d94f4a3bd040e4858317a355c28bd1f73c0c44
jlurgo <jlurgo@gmail.com>
2017-12-19 12:58:54 -0300

    acre

--- ProyectoNet/General/MICOI/EnviadorDeMails.cs
+++ ProyectoNet/General/MICOI/EnviadorDeMails.cs
@@ -13,35 +13,38 @@ namespace General
     {
         public static void EnviarMail(string mail, string titulo, string cuerpo)
         {
-            MailAddress mail_re_recupero = new MailAddress(mail);
+            //MailAddress mail_re_recupero = new MailAddress(mail);
             EnviarMail(new NetworkCredential("rhusuarios", "RRhh2016", "sds_domain_1"),
                     mail,
                    titulo,
                     cuerpo,
                     () => { },
-                    () => { throw new Exception("No se pudo enviar un mail con la clave"); }
+                    () => { throw new Exception("No se pudo enviar el mail"); }
                 );
         }
 
         public static void EnviarMail(NetworkCredential cred, string to, string asunto, string cuerpo, Action on_success, Action on_error)
         {
-            try{
- 	            
+            try
+            {
+
                 MailMessage msg = new MailMessage();
                 msg.To.Add(to);
                 msg.From = new MailAddress("rhusuarios@desarrollosocial.gob.ar");
                 msg.Subject = asunto;
                 msg.IsBodyHtml = true;
                 msg.Body = cuerpo;
-               
-                SmtpClient client = new SmtpClient("owa.desarrollosocial.gob.ar", 25);
+
+                //SmtpClient client = new SmtpClient("owa.desarrollosocial.gob.ar", 25);
+                SmtpClient client = new SmtpClient("10.216.2.65", 25);
                 client.EnableSsl = true;
                 client.UseDefaultCredentials = false;
                 client.Credentials = cred; // Send our account login details to the client.
                 ServicePointManager.ServerCertificateValidationCallback = delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
                 client.Send(msg);          // Send our email.                
             }
-            catch(Exception e){
+            catch (Exception e)
+            {
                 on_error.Invoke();
                 return;
             }
