ee9d8c0031f11d5a7c6675606d4a4e33bb0da56d
acalcagno <calqui@gmail.com>
2018-04-16 20:41:39 -0300

    mend

---
+++ WebAsistencia/WebRH/EvaluacionDesempenio/ABMComites.js
@@ -0,0 +1,12 @@
+﻿var ABMComites = {
+
+    completarDatos: function (comite) {
+        var _this = this;
+        this.ui = $("#contenedor_comites");
+
+        var rh_form = new FormularioBindeado({
+            formulario: this.ui,
+            modelo: comite
+        });
+    }
+}
