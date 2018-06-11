e186f50af50697e2ab8d5338fc79b7695245f684
acalcagno <calqui@gmail.com>
2018-06-11 15:27:55 -0300

    added file missing

---
+++ ProyectoNet/General/MED/DetalleEvaluadosPorUnidadEvaluacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace General.MED
+{
+    public class DetalleEvaluadosPorUnidadEvaluacion
+    {
+        public int Destacados { get; set; }
+        public int Bueno { get; set; }
+        public int Regular { get; set; }
+        public int Deficiente { get; set; }
+        public int Provisoria { get; set; }
+        public int Pendiente { get; set; }
+    }
+}
