219d62211e22569028706d484da70794be231e09
acalcagno <calqui@gmail.com>
2018-10-29 16:44:53 -0300

    -se agrega totalizador a la grilla de ues

--- WebAsistencia/WebRH/EvaluacionDesempenio/ComitesDeEvaluacion.js
+++ WebAsistencia/WebRH/EvaluacionDesempenio/ComitesDeEvaluacion.js
@@ -53,7 +53,7 @@
                 })
         }
 
-        var ue_clicked = function (event) {
+        var checkbox_ue_clicked = function (event) {
 
             var id_ue = event.currentTarget.attributes.model_id.value
             var idComite = spa_tabs.getParam()
@@ -67,6 +67,7 @@
                         alert(err)
                     } else {
                         event.currentTarget.checked = true
+                        calcular_totales_ue()
                     }
                 })
             } else {
@@ -76,9 +77,47 @@
                         alert(err)
                     } else {
                         event.currentTarget.checked = false
+                        calcular_totales_ue()
                     }
                 })
             }
+        }
+
+        var intFromTableCell = function (row, index) {
+            return parseInt(row.children[index].innerText)
+        }
+
+        var calcular_totales_ue = function () {
+            var selected_rows = $("#tabla_unidades input:checked").closest("tr")
+
+            var destacados = 0
+            var bueno = 0
+            var regular = 0
+            var deficiente = 0
+            var provisoria = 0
+            var pendiente = 0
+
+            _.each(selected_rows, row => {
+                destacados += intFromTableCell(row, 2)
+                bueno += intFromTableCell(row, 3)
+                regular += intFromTableCell(row, 4)
+                deficiente += intFromTableCell(row, 5)
+                provisoria += intFromTableCell(row, 7)
+                pendiente += intFromTableCell(row, 8)
+            })
+
+            var totalEvaluados = destacados + bueno + regular + deficiente
+            var totalGeneral = totalEvaluados + provisoria + pendiente
+
+            construir_ue_footer(destacados, bueno, regular, deficiente, provisoria, pendiente, totalEvaluados, totalGeneral) 
+        }
+
+        var construir_ue_footer = function (destacados, bueno, regular, deficiente, provisoria, pendiente, totalEvaluados, totalGeneral) {
+            $(".totalizador").remove()
+            var tr = $('<tr class="totalizador bg-info text-white"><td colspan="2">Total:</td><td>' + destacados + '</td><td>' + bueno + '</td><td>' + regular + '</td><td>' + deficiente + '</td><td>' + totalEvaluados + '</td><td>' + provisoria + '</td><td>' + pendiente + '</td><td>' + totalGeneral + '</td>')
+            tr.children().addClass('text-right')
+            $("#tabla_unidades > tbody").append(tr)
+
         }
 
         var crear_grilla_unidades = function () {
@@ -99,7 +138,7 @@
 
             CreadorDeGrillas('#tabla_unidades', ues_periodo)
 
-            $('[type=checkbox]').click(ue_clicked)
+            $('[type=checkbox]').click(checkbox_ue_clicked)
         }
 
         var on_scr_unidades_enter = function (idComite) {
@@ -114,16 +153,16 @@
 
         //config para la Single Page App con Tabs
         var tabs_events = [{
-                tab_name: '#scr_datos_generales',
-                on_next: on_datos_generales_next,
-                on_enter: on_datos_generales_enter
-            }, {
-                tab_name: '#scr_integrantes',
-                on_enter: on_integrantes_enter
-            }, {
-                tab_name: '#scr_unidades',
-                on_enter: on_scr_unidades_enter
-            }]
+            tab_name: '#scr_datos_generales',
+            on_next: on_datos_generales_next,
+            on_enter: on_datos_generales_enter
+        }, {
+            tab_name: '#scr_integrantes',
+            on_enter: on_integrantes_enter
+        }, {
+            tab_name: '#scr_unidades',
+            on_enter: on_scr_unidades_enter
+        }]
 
         var set_id_periodo_seleccionado = function (e) {
             $("#id_periodo_seleccionado").val(e.currentTarget.attributes.btn_id_periodo.value)
@@ -148,8 +187,6 @@
 
             })
         }
-
-     
 
         var remover_integrante = function (id_integrante, idComite) {
             app_state.DelIntegrante(id_integrante, idComite, i => {
