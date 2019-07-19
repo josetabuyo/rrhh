3aa33cdfc48a2719bb98176b20125ade0f45d659
germanranni <germanrani@gmail.com>
2019-07-18 21:27:54 -0300

    Terminado CIOT

---
+++ WebAsistencia/WebRH/ciot/__Informacion.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ciot_Informacion : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //string pagina = Request.QueryString["pagina"];
+
+        //if (pagina == "que_es_ciot")
+        //{
+        //    Response.Redirect("");
+        //}
+
+
+    }
+}
