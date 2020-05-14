4322d78c980986e7e5abed435e1cad09a29f6cb5
jlurgo <jlurgo@savioke.com>
2020-05-14 19:44:18 -0300

    maquetando la pantalla de asistencia semanal

---
+++ WebAsistencia/WebRH/PotenciarTrabajo/PotenciarTrabajo.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    console.warn('document ready');
+    Backend.start(function () {
+      console.warn('backend started');
+        Backend.PTGetAsistenciasPorGrupoYMes()
+            .onSuccess(function (asistencias) {
+                console.warn('asistencias obtenidas:', asistencias);
+                console.warn($("#pt_tabla_semanal"));
+                $("#pt_tabla_semanal").find(".pt_fila_semanal").remove();
+                _.forEach(asistencias, function (a) {
+                    var fila = $("<tr>")
+
+                    var celda = $("<td>")
+                    celda.text(a.cuil);
+                    fila.append(celda);
+
+                    celda = $("<td>")
+                    celda.text(a.nombre);
+                    fila.append(celda);
+                    
+                    $("#pt_tabla_semanal").append(fila);
+                });
+            })
+            .onError(function (e) {
+                alertify.error("error al obtener asistencias: " + e);
+            });
+    });
+});
