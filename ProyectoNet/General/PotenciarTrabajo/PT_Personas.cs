b06a9c82f04d3e3cb031953748055d68c304f844
germanranni <germanrani@gmail.com>
2020-05-28 22:30:09 -0300

    3 Metodos. Get_Cargar_Combo ("GrupoTrabajo") Get_Periodos Get_Participacion_por_Entidad_Periodo (int idEntidad, string idPeriodo)

---
+++ ProyectoNet/General/PotenciarTrabajo/PT_Personas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace General.PotenciarTrabajo
+{
+    public class PT_Personas
+    {
+        public int Id { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public string Nombre_Apellido { get; set; }
+        public string CUIL { get; set; }
+
+
+    }
+}
