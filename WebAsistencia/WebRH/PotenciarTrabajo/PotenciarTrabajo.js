66d40da1f13b2c1575745c1f9b1d55d4378be71f
jlurgo <jlurgo@savioke.com>
2020-06-04 18:08:44 -0300

    pantallas de carga de asistencia

--- WebAsistencia/WebRH/PotenciarTrabajo/PotenciarTrabajo.js
+++ WebAsistencia/WebRH/PotenciarTrabajo/PotenciarTrabajo.js
@@ -2,27 +2,117 @@
     console.warn('document ready');
     Backend.start(function () {
       console.warn('backend started');
-        Backend.PTGetAsistenciasPorGrupoYMes()
-            .onSuccess(function (asistencias) {
-                console.warn('asistencias obtenidas:', asistencias);
-                console.warn($("#pt_tabla_semanal"));
-                $("#pt_tabla_semanal").find(".pt_fila_semanal").remove();
-                _.forEach(asistencias, function (a) {
-                    var fila = $("<tr>")
-
-                    var celda = $("<td>")
-                    celda.text(a.cuil);
-                    fila.append(celda);
-
-                    celda = $("<td>")
-                    celda.text(a.nombre);
-                    fila.append(celda);
-                    
-                    $("#pt_tabla_semanal").append(fila);
-                });
-            })
-            .onError(function (e) {
-                alertify.error("error al obtener asistencias: " + e);
-            });
+      $("#pt_boton_carga_participacion").click(() => {
+        var seccion = new SeccionEstadoCargaParticipacion();
+        seccion.render();
+        $("#pt_boton_carga_participacion").addClass("pt_selected_section_button");
+      });
     });
 });
+
+class SeccionEstadoCargaParticipacion {
+  constructor() {
+    this.tablaMensual = new TablaParticipacionMensual();
+  }
+
+  render () {
+    $("#pt_estado_semanal").hide();
+    $("#pt_estado_mensual").show();
+    Backend.PTGetEstadoCargaParticipacionPorPeriodo("1")
+      .onSuccess((estados) => {
+          console.warn('estados obtenidos:', estados);
+          this.tablaMensual.render(estados);
+      })
+      .onError(function (e) {
+          this.alertify.error("error al obtener asistencias: " + e);
+      });
+  }
+}
+
+class TablaPT {
+  agregarCeldaTextoAFila(fila, contenido) {
+    var celda = $("<td>")
+    celda.text(contenido);
+    fila.append(celda);
+  }
+}
+
+class TablaParticipacionMensual extends TablaPT{
+  constructor () {
+    super();
+    this.tablaSemanal = new TablaParticipacionSemanal();
+  }
+
+  render (estados) {
+    $("#pt_tabla_participacion_mensual").find(".pt_fila_participacion_mensual").remove();
+    _.forEach(estados, (e) => {
+      const fila = $("<tr>")
+      this.agregarCeldaTextoAFila(fila, e.nombreGrupoTrabajo);
+      this.agregarCeldaTextoAFila(fila, e.activos);
+      this.agregarCeldaTextoAFila(fila, e.suspendidos);
+      this.agregarCeldaTextoAFila(fila, e.incompatibles);
+      this.agregarCeldaTextoAFila(fila, e.activos + e.suspendidos + e.incompatibles);
+      this.agregarCeldaTextoAFila(fila, e.sinCarga);
+      this.agregarCeldaTextoAFila(fila, e.parciales);
+
+      const celda = $("<td>")
+      celda.text(e.completos);
+      const icono_lista = $("<img>");
+      icono_lista.attr("src", "IconoLista.png");
+      icono_lista.addClass("pt_icono_lista");
+      icono_lista.click(() => {
+        Backend.PTGetAsistenciasPorGrupoYPeriodo(e.idGrupoTrabajo)
+          .onSuccess((personas) => {
+              $("#pt_estado_mensual").hide();
+              $("#pt_estado_semanal").show();
+              this.tablaSemanal.render(personas);
+          })
+          .onError(function (e) {
+              this.alertify.error("error al obtener asistencias: " + e);
+          });
+      });
+      celda.append(icono_lista);
+      fila.append(celda);
+
+      this.agregarCeldaTextoAFila(fila, e.conInforme);
+      fila.addClass("pt_fila_participacion_mensual");
+      $("#pt_tabla_participacion_mensual").append(fila);
+    });
+  }
+}
+
+class TablaParticipacionSemanal {
+  render (personas) {
+    $("#pt_tabla_participacion_semanal").find(".pt_fila_participacion_semanal").remove();
+    _.forEach(personas, (p) => {
+      var fila = $("<tr>")
+
+      var celda = $("<td>")
+      celda.text(p.cuil);
+      fila.append(celda);
+
+      celda = $("<td>")
+      celda.text(p.nombre);
+      fila.append(celda);
+
+      this.renderComboAsistencia(fila, p.asistSemana1);
+      this.renderComboAsistencia(fila, p.asistSemana2);
+      this.renderComboAsistencia(fila, p.asistSemana3);
+      this.renderComboAsistencia(fila, p.asistSemana4);
+
+      celda = $("<td>")
+      celda.text(p.observaciones);
+      fila.append(celda);
+      fila.addClass("pt_fila_participacion_semanal");
+      $("#pt_tabla_participacion_semanal").append(fila);
+    });
+  }
+
+  renderComboAsistencia (fila, asistencia) {
+    var celda = $("<td>")
+    var combo_semanal = $("#pt_plantillas").find(".cmb_porcentaje_asistencia").clone();
+    combo_semanal.val(asistencia === '' ? '100' : asistencia);
+    celda.append(combo_semanal);
+    fila.append(celda);
+  }
+}
--- WebAsistencia/WebRH/Scripts/MockBackend.js
+++ WebAsistencia/WebRH/Scripts/MockBackend.js
@@ -2,25 +2,63 @@ var Backend = {
     start: function (on_ready) {
         setTimeout(on_ready, 100);
     },
-    PTGetAsistenciasPorGrupoYMes (grupo, mes) {
-        var data = [
-          {cuil: '20-41342135-9', nombre: 'PEREZ, Juan Manuel',
-           asistSemana1: '100', asistSemana2: '100', asistSemana3: '100',
-           asistSemana4: '100', observaciones: 'bla bla'},
-          {cuil: '20-41384535-7', nombre: 'GONZALEZ, Maria Jimena',
-           asistSemana1: '0', asistSemana2: '', asistSemana3: '75',
-           asistSemana4: '100', observaciones: 'bla bla'},
-          {cuil: '20-42344135-3', nombre: 'SCHMIDT, Marisa Paola',
-           asistSemana1: '100', asistSemana2: '50', asistSemana3: '100',
-           asistSemana4: '100', observaciones: 'bla bla'},
-          {cuil: '20-42344135-3', nombre: 'SCHMIDT, Marisa Paola',
-           asistSemana1: '', asistSemana2: '', asistSemana3: '',
-           asistSemana4: '', observaciones: ''},
-        ]
-        var promesa = new Promesa();
-        setTimeout(function() {
-          promesa.success(data);
-        }, 100);
-        return promesa;
+    PTGetEstadoCargaParticipacionPorPeriodo (id_periodo) {
+        return this.ReturnAsync([
+          { idGrupoTrabajo: '1', nombreGrupoTrabajo: 'Los tigres de tigre',
+            activos: 115, suspendidos: 16, incompatibles: 16,
+            sinCarga: 0, parciales: 0, completos: 0, conInforme: 10},
+          { idGrupoTrabajo: '2', nombreGrupoTrabajo: 'Los caniches de Domingo',
+            activos: 25, suspendidos: 0, incompatibles: 0,
+            sinCarga: 0, parciales: 0, completos: 0, conInforme: 25},
+          { idGrupoTrabajo: '3', nombreGrupoTrabajo: 'Todos Juntos Podemos',
+            activos: 20, suspendidos: 6, incompatibles: 2,
+            sinCarga: 0, parciales: 0, completos: 0, conInforme: 20},
+          { idGrupoTrabajo: '4', nombreGrupoTrabajo: 'Esperanza',
+            activos: 20, suspendidos: 5, incompatibles: 5,
+            sinCarga: 0, parciales: 0, completos: 0, conInforme: 20},
+          { idGrupoTrabajo: '5', nombreGrupoTrabajo: 'Lealtad',
+            activos: 25, suspendidos: 3, incompatibles: 2,
+            sinCarga: 0, parciales: 0, completos: 0, conInforme: 25},
+          { idGrupoTrabajo: '6', nombreGrupoTrabajo: 'Compromiso',
+            activos: 25, suspendidos: 2, incompatibles: 2,
+            sinCarga: 0, parciales: 0, completos: 0, conInforme: 25},
+        ]);
+    },
+    PTGetAsistenciasPorGrupoYPeriodo (id_grupo, id_periodo) {
+        return this.ReturnAsync([
+          { cuil: '20-41342135-9', nombre: 'PEREZ, Juan Manuel',
+             asistSemana1: '100', asistSemana2: '100', asistSemana3: '100',
+             asistSemana4: 'medico', asistSemana5: null, observaciones: 'bla bla'},
+          {  cuil: '20-41384535-7', nombre: 'GONZALEZ, Maria Jimena',
+            asistSemana1: '0', asistSemana2: '', asistSemana3: '75',
+            asistSemana4: '100', asistSemana5: null, observaciones: 'bla bla'},
+          { cuil: '20-42344135-3', nombre: 'SCHMIDT, Marisa Paola',
+            asistSemana1: '100', asistSemana2: '50', asistSemana3: '100',
+            asistSemana4: '100', asistSemana5: null, observaciones: 'bla bla'},
+          { cuil: '20-45344115-4', nombre: 'VIÑATE, Fabiana Carmen',
+            asistSemana1: '', asistSemana2: '', asistSemana3: '',
+            asistSemana4: '', asistSemana5: null, observaciones: ''},
+          { cuil: '20-42124135-5', nombre: 'LOPEZ, Alberto Mario',
+            asistSemana1: '', asistSemana2: '', asistSemana3: '',
+            asistSemana4: '', asistSemana5: null, observaciones: ''},
+          { cuil: '20-42345135-6', nombre: 'MARINI, Pedro Pablo',
+            asistSemana1: '', asistSemana2: '', asistSemana3: '',
+            asistSemana4: '', asistSemana5: null, observaciones: ''},
+        ]);
+    },
+    PTGetPeriodos() {
+      return this.ReturnAsync([
+        {id: '1', descripcion: 'Abril 2020', cantSemanas: 4}
+      ]);
+    },
+    PTGuardarAsistenciasGrupo(asistencias) {
+
+    },
+    ReturnAsync(data) {
+      var promesa = new Promesa();
+      setTimeout(function() {
+        promesa.success(data);
+      }, 100);
+      return promesa;
     }
 };
